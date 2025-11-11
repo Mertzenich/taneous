6dcfe54ef5bd1b5b8611f39e6d0e93a5451ad373
Adam Mertzenich <adam+git@mertzeni.ch>
2025-11-11 11:58:04 -0600

    chore: boilerplate

--- src/taneous/core.cljs
+++ src/taneous/core.cljs
@@ -1,4 +1,26 @@
-(ns taneous.core)
+(ns taneous.core
+  (:require ["@instantdb/admin" :refer [init id]]))
+
+(def APP_ID js/process.env.INSTANT_APP_ID)
+
+(def APP_SECRET js/process.env.INSTANT_APP_SECRET)
+
+(comment
+
+  (def db (init #js {:appId APP_ID
+                     :adminToken APP_SECRET}))
+
+  (def data (.query db #js {:goals #js {}
+                            :todos #js {}}))
+
+  (.then (.query db #js {:goals #js {}
+                         :todos #js {}})
+         #(js/console.log (.-todos %)))
+
+  (def goals (.-goals data))
+  (def todos (.-todos data))
+
+  '...)
 
 (defn -main
   [& _args]
